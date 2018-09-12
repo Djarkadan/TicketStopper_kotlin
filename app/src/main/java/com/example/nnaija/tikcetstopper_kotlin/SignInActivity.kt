398e12d98124600de50ccda24fb57a60cbc8df00
Nizar Naija <naija.nizar@gmail.com>
2018-09-12 15:54:05 -0400

    Sign In Fragment and Register fragement added with lateral navigation between them(tab) The tab detects the page change event but is not yet able to switch between fragments  through a tabClick

--- app/src/main/java/com/example/nnaija/tikcetstopper_kotlin/SignInActivity.kt
+++ app/src/main/java/com/example/nnaija/tikcetstopper_kotlin/SignInActivity.kt
@@ -10,42 +10,68 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import android.R.attr.data
+import android.content.Context
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.common.api.ApiException
 import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.widget.ViewUtils
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import java.util.*
 
 
-class SignInActivity : AppCompatActivity(),View.OnClickListener {
+class SignInActivity : AppCompatActivity() {
+
 
     lateinit var  googleSignInClient:GoogleSignInClient
+
+
+    lateinit var vpSign:ViewPager
     val RC_SIGN_IN=100
     val TAG="SignIn Activity"
 
 
-    override fun onClick(v: View) {
+//    override fun onClick(v: View) {
+//
+//        when(v.id){
+//            R.id.btnGoogleSignIn->signIn()
+//        }
+//
+//    }
 
-        when(v.id){
-            R.id.btnGoogleSignIn->signIn()
-        }
-
-    }
-
-    private fun signIn() {
-        val signInIntent=googleSignInClient.signInIntent
-        startActivityForResult(signInIntent,RC_SIGN_IN)
-    }
+//    private fun signIn() {
+//        val signInIntent=googleSignInClient.signInIntent
+//        startActivityForResult(signInIntent,RC_SIGN_IN)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-       val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail().build();
 
-         googleSignInClient=GoogleSignIn.getClient(this,gso)
-        btnGoogleSignIn.setOnClickListener(this);
+
+//
+//       val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail().build();
+//
+//         googleSignInClient=GoogleSignIn.getClient(this,gso)
+//        btnGoogleSignIn.setOnClickListener(this)
+
+        val pagerAdpater: PagerAdapter
+        pagerAdpater = PagerAdapter(supportFragmentManager)
+        vpSign=findViewById(R.id.vpSign)
+        vpSign.adapter=pagerAdpater
+
+        vpSign.addOnPageChangeListener(object:TabLayout.TabLayoutOnPageChangeListener(tlSign){})
+
     }
 
 
@@ -75,4 +101,59 @@ class SignInActivity : AppCompatActivity(),View.OnClickListener {
         if(account!=null){
         }
     }
+
+    class PagerAdapter(fm:FragmentManager):FragmentPagerAdapter(fm){
+        override fun getCount(): Int {
+            return 2
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return when(position){
+                1-> MyFragment.newInstance(R.layout.register_tab_fragment)
+                else ->  {
+                    MyFragment.newInstance(R.layout.sign_in_tab_fragment)
+                }
+
+            }
+        }
+
+
+
+    }
+
+      class MyFragment :Fragment(){
+           var  currentFragmentLayout:Int=R.layout.sign_in_tab_fragment
+          val idBundelString="id"
+          val signInFragmentId=R.layout.sign_in_tab_fragment
+          val registerFragmentId=R.layout.register_tab_fragment
+          companion object {
+
+
+              fun newInstance(id: Int):MyFragment{
+
+                  val fragment=MyFragment()
+                  val args=Bundle()
+                  args.putInt(fragment.idBundelString,id)
+                  fragment.arguments=args
+                  return fragment
+              }
+          }
+
+          override fun onCreate(savedInstanceState: Bundle?) {
+              super.onCreate(savedInstanceState)
+
+               currentFragmentLayout=arguments?.getInt(idBundelString)?:signInFragmentId
+
+
+
+          }
+
+          override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+               super.onCreateView(inflater, container, savedInstanceState)
+              val view=inflater.inflate(currentFragmentLayout,container,false)
+
+              return view
+
+          }
+      }
 }
